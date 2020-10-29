efa2548d73a12546434b8e2219e33065e991bf92
Ergun Oz <ergun.oz82@gmail.com>
2020-10-29 18:07:05 -0400

    created a boardactions for delete and update deleted 2 components becuase i thought to delete and update in different way

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,9 @@ import Navbar from 'react-bootstrap/Navbar'
 
 const authenticatedOptions = (
   <Fragment>
+    <Nav.Link href="#create-board">Add Board</Nav.Link>
     <Nav.Link href="#user-board">My Boards</Nav.Link>
-    <Nav.Link href="#boards">Boards</Nav.Link>
-    <Nav.Link href="#create-board">Create Board</Nav.Link>
+    <Nav.Link href="#boards">All Boards</Nav.Link>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
   </Fragment>
